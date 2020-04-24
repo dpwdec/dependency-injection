1a6f714a2b7120109ab43dfc0c5d1ec7169bc310
dpwdec <declankolakowski@gmail.com>
2020-04-24 10:39:36 +0100

    refactored exercise 3 solution to use let

--- spec/exercise_3_spec.rb
+++ spec/exercise_3_spec.rb
@@ -1,17 +1,14 @@
 require 'exercise_3'
 
 describe SayHello do
+
+  let(:message) { double("message") }
+  let(:email) { double("email", message: message) }
+  let(:email_class) { double("email class", new: email) }
+  let(:subject) { described_class.new(email_class) }
+
   describe '#run' do
     it 'sends a message' do
-      message = double("message")
-
-      # only need message class double if testing EmailClient
-      #message_class = double("message class", new: email)
-
-      email = double("email", message: message)
-      email_class = double("email class", new: email)
-
-      subject = described_class.new(email_class)
       expect(message).to receive(:send)
       subject.run
     end
